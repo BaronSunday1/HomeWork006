7f0f1f082ba6df72003ff1842d526c794d9af26e
Yaroslav <tarasdmb2008@mail.ru>
2023-03-03 19:35:33 +0500

    second task

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,18 +1,43 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
-
-Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
+int Task(string text)
 {
-    if (arr[i] > 0)
+    System.Console.WriteLine(text);
+    string number = Console.ReadLine();
+    int result = Convert.ToInt32(number);
+    return result;
+}
+int EnterArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
     {
-        count++;
+        array[i] = Task($"Введите {i + 1} элемент");
+    }
+    return array;
+}
+void PrintResult(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"[{i}] = {array[i]}");
     }
 }
- 
-Console.WriteLine($"Кол-во элементов > 0: {count}");
-
+int MoreThanZero(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+int size = Task("Введите элементы(через пробел): ");
+int[] array;
+array = EnterArray(size);
+PrintResult(array);
+System.Console.WriteLine($"The number of numbers greater than zero is equal to = {MoreThanZero(array)}");
